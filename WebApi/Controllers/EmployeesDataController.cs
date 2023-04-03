09d110b7d0dc65d73d35f7af39b23dd079c66128
htadmin <htadmin@DESKTOP-IFH26PN>
2023-04-03 11:42:17 +0530

    Add project files.

---
+++ WebApi/Controllers/EmployeesDataController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class EmployeesDataController : ApiController
+    {
+        static List<string> Employees =new List<string> { "Aman", "Ram", "singh","kunal","amit" };
+        // GET Api/EmployeesData
+        public IEnumerable<string> GetEmployees()
+        {
+            return Employees;
+        }
+        //GET Api/EmployeesData/{id}
+        public string GetEmployeesByIndex(int id)
+        {
+            return Employees[id];
+        }
+        //POST Api/EmployeesData
+        public void Post([FromBody]string value)
+        {
+            Employees.Add(value);
+        }
+        public void Put(int id, [FromBody]string value)
+        {
+            Employees[id] = value;
+        }
+        public void Delete(int id)
+        {
+            Employees.RemoveAt(id);
+        }
+
+ 
+    }
+
+
+}
